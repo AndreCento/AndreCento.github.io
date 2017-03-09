7efa52364d32faf0e2189a654efa8dff5945ec46
AndreCento <sonoandre11@gmail.com>
2017-03-09 19:27:29 +0000

    final

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
-$(function(){
 
-  $('.buy-button').on('click.buy', function(){
-
-
-    var $this = $(this);
-
-    var price = $this.data('price');
-    var $basketDisplay = $('#basket-total');
-    var basketTotal = parseFloat($basketDisplay.text());
-    var newBasketTotal= basketTotal + price;
-
-    $basketDisplay.text(newBasketTotal);
-
-    console.log('price', price);
-    console.log('basketTotal', basketTotal);
-    console.log('newBasketTotal', newBasketTotal);
-  });
-});
